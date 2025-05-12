95bf3429ad747f2b4c6c96e2f7f5130bac7d1121
bartoszkrupa5@gmail.com <bartoszkrupa5@gmail.com>
2025-05-12 17:53:56 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,28 +18,67 @@ export class AppComponent implements OnInit{
     this.setLessons();
   }
 
+  /*
+  12
+  8
+  17
+  6
+  21
+  12
+  15
+  7
+  19
+  8
+  17
+  6
+  19
+  3
+
+  */
   public setLessons(){
+
     this.lessons = [
-      {name: "Algorytmy danych", type: LessonType.cwiczenia, where: 109, when: "2-12,14", day: DayType.Poniedzialek, length: 2, start: 1},
-      {name: "Elektrotechnika", type: LessonType.cwiczenia, where: 9, when: "2-12,14", day: DayType.Poniedzialek, length: 2, start: 3},
-      {name: "Języki paradygmaty", type: LessonType.cwiczenia, where: 212, when: "2-12,14", day: DayType.Poniedzialek, length: 2, start: 5},
-      {name: "Podstawy elektroniki", type: LessonType.wyklad, where: 209, when: "2,4,6,8,10,12,14", day: DayType.Poniedzialek, length: 2, start: 7},
-      {name: "ANG", type: LessonType.lektorat, where: 209, when: "2,4,5,8,10,12,14", day: DayType.Poniedzialek, length: 1, start: 9},
-      {name: "Podstawy elektroniki", type: LessonType.wyklad, where: 209, when: "4", day: DayType.Poniedzialek, length: 1, start: 9},
-      {name: "ANG", type: LessonType.lektorat, where: 115, when: "1,2,3,5,7,9,11,12,14", day: DayType.Poniedzialek, length: 1, start: 10},
-      {name: "ANG", type: LessonType.lektorat, where: 115, when: "1,3,5,7,11,14", day: DayType.Poniedzialek, length: 1, start: 11},
+      {name: "Programowanie komponentowe i rozproszon", type: LessonType.wyklad, where: 210, when: "1-14", day: DayType.Poniedzialek, length: 1, start: 10},
+      {name: "Programowanie komponentowe i rozproszon", type: LessonType.wyklad, where: 210, when: "1", day: DayType.Poniedzialek, length: 1, start: 11},
+      {name: "Programowanie komponentowe i rozproszon", type: LessonType.cwiczenia, where: 210, when: "2-13", day: DayType.Poniedzialek, length: 1, start: 11},
 
-      {name: "Sieciowe", type: LessonType.wyklad, where: 207, when: "2,5,9,14", day: DayType.Wtorek, length: 2, start: 1},
-      {name: "Sieciowe", type: LessonType.cwiczenia, where: 207, when: "1,3-8,10-13", day: DayType.Wtorek, length: 2, start: 1},
-      {name: "WF", type: LessonType.cwiczenia, where: 112, when: "2-13", day: DayType.Wtorek, length: 1, start: 4},
-      {name: "Sieciowe", type: LessonType.cwiczenia, where: 111, when: "2", day: DayType.Wtorek, length: 2, start: 5},
+      {name: "Sieci neuronowe", type: LessonType.cwiczenia, where: 0, when: "2-12", day: DayType.Poniedzialek, length: 2, start: 13},
+      /**/
 
-      {name: "Technika cyfrowa", type: LessonType.wyklad, where: 207, when: "2,4,5,8,10,12,14", day: DayType.Sroda, length: 2, start: 2},
-      {name: "Technika cyfrowa", type: LessonType.wyklad, where: 207, when: "4", day: DayType.Sroda, length: 1, start: 4},
-      {name: "Algorytmy danych", type: LessonType.wyklad, where: 207, when: "1-3,5,6,9,11,13", day: DayType.Sroda, length: 1, start: 4},
+      {name: "Systemy wbudowane", type: LessonType.wyklad, where: 207, when: "1,3,5,7,9,11,13", day: DayType.Wtorek, length: 1, start: 1},
+      {name: "Systemy wbudowane", type: LessonType.wyklad, where: 207, when: "1,3,5,6,7,9,11,13", day: DayType.Wtorek, length: 1, start: 2},
 
-      {name: "Języki paradygmaty", type: LessonType.wyklad, where: 207, when: "3", day: DayType.Piatek, length: 1, start: 6},
-      {name: "Języki paradygmaty", type: LessonType.wyklad, where: 207, when: "1,3,5,7,9,11,13", day: DayType.Piatek, length: 1, start: 7},
+      {name: "Systemy wbudowane", type: LessonType.cwiczenia, where: 108, when: "2,4,6,10,12,14", day: DayType.Wtorek, length: 2, start: 5},
+
+      {name: "Grafika komputerowa i wizualizacja", type: LessonType.wyklad, where: 107, when: "1,3,5,7,8,9,11,13", day: DayType.Wtorek, length: 1, start: 5},
+      {name: "Grafika komputerowa i wizualizacja", type: LessonType.wyklad, where: 107, when: "1,3,5,7,9,11,13", day: DayType.Wtorek, length: 1, start: 6},
+
+      {name: "Grafika komputerowa i wizualizacja", type: LessonType.cwiczenia, where: 216, when: "1,3,5,9,11,13", day: DayType.Wtorek, length: 2, start: 7},
+      
+      /**/ 
+      {name: "Seminarium dyplomowe", type: LessonType.cwiczenia, where: 207, when: "5,7,9,11,13", day: DayType.Sroda, length: 2, start: 1},
+
+      {name: "Badania Operacyjne", type: LessonType.wyklad, where: 207, when: "2,3,5,7,9,11,13", day: DayType.Sroda, length: 1, start: 3},
+
+      {name: "Badania Operacyjne", type: LessonType.wyklad, where: 207, when: "2", day: DayType.Sroda, length: 1, start: 4},
+      {name: "Badania Operacyjne", type: LessonType.cwiczenia, where: 207, when: "3,5,7,9,11,13", day: DayType.Sroda, length: 1, start: 4},
+
+      {name: "Badania Operacyjne", type: LessonType.cwiczenia, where: 207, when: "3,5,7,9,11,13", day: DayType.Sroda, length: 1, start: 5},
+
+
+      /**/ 
+      {name: "Projekt indywidualny", type: LessonType.cwiczenia, where: 108, when: "1,5,6", day: DayType.Piatek, length: 2, start: 2},
+      {name: "Projekt indywidualny", type: LessonType.cwiczenia, where: 108, when: "1,3,5-7,9,11,13", day: DayType.Piatek, length: 2, start: 4},
+
+      {name: "Podstawy przedsiębiorczości", type: LessonType.wyklad, where: 116, when: "4,7,11,12", day: DayType.Piatek, length: 2, start: 6},
+      {name: "Elementy sztucznej inteligencji", type: LessonType.wyklad, where: 108, when: "2,3,5,6,8,10,13", day: DayType.Piatek, length: 2, start: 6},
+      {name: "Projekt indywidualny", type: LessonType.cwiczenia, where: 108, when: "9", day: DayType.Piatek, length: 2, start: 6},
+
+      {name: "Elementy sztucznej inteligencji", type: LessonType.cwiczenia, where: 210, when: "3,5,8,9,10,13", day: DayType.Piatek, length: 1, start: 8},
+      {name: "Elementy sztucznej inteligencji", type: LessonType.wyklad, where: 116, when: "6", day: DayType.Piatek, length: 1, start: 8},
+
+      {name: "Elementy sztucznej inteligencji", type: LessonType.cwiczenia, where: 210, when: "3,5,8-10,13", day: DayType.Piatek, length: 1, start: 9},
+
     ];
 
     for(let item of this.lessons){
--- src/app/components/time-table/time-table.component.ts
+++ src/app/components/time-table/time-table.component.ts
@@ -10,13 +10,13 @@ export class TimeTableComponent implements OnInit {
   @Input()
   public events: LessonData[] = [];
   public possibleHours: any[] = [];
-  public posibleDate: any[] = [0,1,2,3,4];
+  public posibleDate: any[] = [0,1,2,4];
   public choosenWeek: number = 1;
 
   constructor() { }
 
   ngOnInit(): void {
-    let startDay = new Date("2023-10-02");
+    let startDay = new Date("2025-02-21T16:15:00");
     let endDay = new Date();
 
     let time = endDay.getTime() - startDay.getTime();
@@ -35,14 +35,18 @@ export class TimeTableComponent implements OnInit {
       {start: "15:15", end: "16:00", index: 9},
       {start: "16:15", end: "17:00", index: 10},
       {start: "17:05", end: "17:50", index: 11},
+      {start: "18:05", end: "18:50", index: 12},
+      {start: "18:40", end: "19:25", index: 13},
+      {start: "19:30", end: "20:15", index: 14},
     ]
+
+
   }
 
   public getEvent(dateNumber, hoursNumber){
     let items = this.events.filter(eve => eve.when.find(eve2 => eve2 == this.choosenWeek));
     let items2 = items.filter(eve => eve.day==dateNumber);
     let items3 = items2.find(eve => eve.start == hoursNumber);
- 
     if(items3){
       return items3;
     } else {
@@ -52,6 +56,10 @@ export class TimeTableComponent implements OnInit {
       }
     }
     return null;
+  }
+
+  public hasEventsForRow(hoursNumber: number): boolean {
+    return this.posibleDate.some(date => this.getEvent(date, hoursNumber));
   }
 
 }
